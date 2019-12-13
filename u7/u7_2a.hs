692b6d8109b727163b16d38c54095e164b8a96eb
Trollport <48841354+Trollport@users.noreply.github.com>
2019-12-13 07:00:38 +0100

    u7_3c.hs hinzugefügt

--- u7/u7_2a.hs
+++
@@ -1,5 +0,0 @@
-binom1 :: Integer -> Interger -> Integer
-binom1 n k
---binom1 0 0 = 1 --patternmatching fuer n= 0 k=0 return 1
-	|n==0 && k == 0 = 1
-	|otherwise = (fakultaet(n)/(fakultaet(k)*fakultaet(n-k))--fakultaet funktion ist noch zu erstellen
---
+++ u7/u7_3c.hs
@@ -0,0 +1,16 @@
+import Data.Char
+caesar5::Char->Char
+caesar5 a
+	 | Data.Char.isLower a && Data.Char.ord a + 5 < 123 = Data.Char.chr (Data.Char.ord a + 5) --klein und im Bereich
+	 | Data.Char.isLower a && Data.Char.ord a + 5 >= 123 = Data.Char.chr (Data.Char.ord a - 21) --klein und zurueckspringen
+	 | Data.Char.isUpper a && Data.Char.ord a + 5 < 91 = Data.Char.chr (Data.Char.ord a + 5) --gross und im Bereich
+	 | Data.Char.isUpper a && Data.Char.ord a + 5 >= 91 = Data.Char.chr (Data.Char.ord a - 21) --gross und zurueckspringen
+	 | otherwise = error "Eingabe ist weder ein ASCII klein Buchstabe noch ein gross Buchstabe"
+	 
+jTwistedCaesar::Int->Char->Char
+jTwistedCaesar j c
+	 | Data.Char.isLower c && Data.Char.ord c + j < 123 = toUpper (Data.Char.chr (Data.Char.ord c + j)) --klein und im Bereich
+	 | Data.Char.isLower c && Data.Char.ord c + j >= 123 = toUpper (Data.Char.chr (Data.Char.ord c - 26 + j)) --klein und zurueckspringen
+	 | Data.Char.isUpper c && Data.Char.ord c + j < 91 = toLower (Data.Char.chr (Data.Char.ord c + j)) --gross und im Bereich
+	 | Data.Char.isUpper c && Data.Char.ord c + j >= 91 = toLower (Data.Char.chr (Data.Char.ord c - 26 + j)) --gross und zurueckspringen
+	 | otherwise = error "Eingabe ist weder ein ASCII klein Buchstabe noch ein gross Buchstabe"
